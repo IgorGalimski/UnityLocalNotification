515c732e62e8ee42b4e9d530a49e8f1e5014d898
IgorGalimski <igorg@playtika.com>
2020-08-27 18:18:26 +0300

    Add base folders

---
+++ Assets/Scripts/OnPostprocessBuild.cs
@@ -0,0 +1,25 @@
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+namespace UnityLocalNotification
+{
+    public class OnPostprocessBuild
+    {
+        [PostProcessBuild]
+        public static void OnPostprocessBuildHandler(BuildTarget buildTarget, string path)
+        {
+            var projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+   
+            var proj = new PBXProject ();
+            proj.ReadFromString (File.ReadAllText (projPath));
+   
+            var target = proj.TargetGuidByName ("Unity-iPhone");
+
+            proj.AddFrameworkToProject(target, "UserNotifications.framework", true);
+
+            File.WriteAllText (projPath, proj.WriteToString ());
+        }
+    }
+}
