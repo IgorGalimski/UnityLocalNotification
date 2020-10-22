95ddca313af6a97bbb4885c1562cc7ec16b5d6b6
IgorGalimski <igorg@playtika.com>
2020-10-22 21:45:39 +0300

    Add set broadcast receiver

--- Assets/UnityLocalNotification/Scripts/Editor/GradlePostBuildProcessor.cs
+++ Assets/UnityLocalNotification/Scripts/Editor/GradlePostBuildProcessor.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Xml;
 using UnityEditor.Android;
 using UnityEngine;
 
@@ -8,6 +9,9 @@ namespace UnityLocalNotification.Scripts.Editor
 {
     public class GradlePostBuildProcessor : IPostGenerateGradleAndroidProject
     {
+        private const string ANDROID_NAMESPACE_URI = "http://schemas.android.com/apk/res/android";
+        private const string NOTIFICATION_BROADCAST_RECEIVER = "com.igorgalimski.unitylocalnotification.NotificationBroadcastReceiver";
+        
         public int callbackOrder => 999;
 
         public void OnPostGenerateGradleAndroidProject(string path)
@@ -15,12 +19,12 @@ namespace UnityLocalNotification.Scripts.Editor
             Debug.Log("Bulid path : " + path);
             
             UpdateBuildGradle(path);
+            UpdateAndroidManifest(path);
         }
 
         private void UpdateBuildGradle(string path)
         {
-            
-            string buildGradleFile = path + "/build.gradle";
+            var buildGradleFile = path + "/build.gradle";
 
             var gradle = File.ReadAllLines(buildGradleFile).ToList();
 
@@ -33,6 +37,23 @@ namespace UnityLocalNotification.Scripts.Editor
                 
                 File.WriteAllLines(buildGradleFile, gradle);
             }
+        }
+
+        private void UpdateAndroidManifest(string path)
+        {
+            var manifestPath = path + "/src/main/AndroidManifest.xml";
+            
+            var manifestDoc = new XmlDocument();
+            manifestDoc.Load(manifestPath);
+            
+            var applicationXmlNode = manifestDoc.SelectSingleNode("manifest/application");
+            
+            var notificationManagerReceiver = manifestDoc.CreateElement("receiver");
+            notificationManagerReceiver.SetAttribute("name", ANDROID_NAMESPACE_URI, NOTIFICATION_BROADCAST_RECEIVER);
+
+            applicationXmlNode.AppendChild(notificationManagerReceiver);
+            
+            manifestDoc.Save(manifestPath);
         }
     }
 }
