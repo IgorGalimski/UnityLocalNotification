feed149bc4d72dde41e014e1fcd3ce66fdfa746e
IgorGalimski <igorg@playtika.com>
2021-01-22 09:54:59 +0300

    Add package creator

---
+++ Assets/UnityLocalNotification/Scripts/Editor/PackageCreator.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace UnityLocalNotification.Scripts.Editor
+{
+    public class PackageCreator
+    {
+        private static readonly string[] AssetList = AssetDatabase.FindAssets(
+            "*", new[]
+            {
+                "Assets/UnityLocalNotification/Plugins",
+                "Assets/UnityLocalNotification/Scripts",
+            });
+
+        [MenuItem("Assets/Export Package")]
+        public static void ExportPackage()
+        {
+            var assetList = new List<string>();
+            
+            foreach (var guid in AssetList)
+            {
+                var assetPath = AssetDatabase.GUIDToAssetPath(guid);
+                assetList.Add(assetPath);
+            }
+
+            AssetDatabase.ExportPackage(assetList.ToArray(), "unity_local_notification.unitypackage", ExportPackageOptions.Recurse);
+        }
+    }
+}
